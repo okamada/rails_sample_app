8ecbb9df15ccdccff57741270eb3e1062b0ec47a
Hisashi Kamada <kamada.1994@gmail.com>
2023-10-09 10:25:35 +0000

    Change minimum length of password from 6 to 8

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,8 +5,8 @@ class UserTest < ActiveSupport::TestCase
     @user = User.new(
       name:                   "Example User", 
       email:                  "user@example.com",
-      password:               "foobar",
-      password_confirmation:  "foobar"
+      password:               "a" * 8,
+      password_confirmation:  "a" * 8
     )
   end
 
@@ -77,12 +77,12 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "password should be present (nonblank)" do
-    @user.password = @user.password_confirmation = " " * 6
+    @user.password = @user.password_confirmation = " " * 8
     assert_not @user.valid?
   end
 
   test "password should have a minimum length" do
-    @user.password = @user.password_confirmation = "a" * 5
+    @user.password = @user.password_confirmation = "a" * 7
     assert_not @user.valid?
   end
 end
