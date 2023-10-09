9af6a2fc9842bfbeb2937c16be62b5f8f0f75057
Hisashi Kamada <kamada.1994@gmail.com>
2023-10-09 10:18:57 +0000

    Add columns about password to Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   validates :name,  presence: true, length: { maximum: 50  }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i # simple validation
   validates :email, presence: true, length: { maximum: 255 }, format: VALID_EMAIL_REGEX, uniqueness: true
+  has_secure_password
 end
