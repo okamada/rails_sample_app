f23124d2fca9659521e72a22441929c303fc9d2d
Hisashi Kamada <kamada.1994@gmail.com>
2023-10-10 14:18:46 +0000

    add session control

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # TODO ログイン後にユーザー情報のページにリダイレクトする
+      reset_session # ログインの直前に必ず書く処理 ∵セッション固定攻撃への対策
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new', status: :unprocessable_entity
