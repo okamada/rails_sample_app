f2a07a34e5dbdd2e748e81bb297096b29805bc7f
Hisashi Kamada <kamada.1994@gmail.com>
2023-10-15 11:36:43 +0000

    change header and footer to be applicatiable to mobile

--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -2,6 +2,14 @@
 
 // Toggle Listenerを追加してClickをListenする。
 document.addEventListener("turbo:load", function() {
+
+  let hamburger = document.querySelector("#hamburger");
+  hamburger.addEventListener("click", function(event) {
+    event.preventDefault();
+    let menu = document.querySelector("#navbar-menu");
+    menu.classList.toggle("collapse");
+  });
+
   let account = document.querySelector("#account");
   account.addEventListener("click", function(event) {
     event.preventDefault();
