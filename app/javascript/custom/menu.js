a56599c1e71526230ee5287dc970595da31d92b7
Hisashi Kamada <kamada.1994@gmail.com>
2023-10-15 12:31:19 +0000

    fix menu.js bug

--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -1,16 +1,16 @@
 // メニュー操作
 
 function addToggleListener(selected_id, menu_id, toggle_class) {
-  let selected_element= document.querySelector(`#${selected_id}`);
-  selected_element.addEventListener("click", function(event){
+  let selected_element = document.querySelector(`#${selected_id}`);
+  selected_element.addEventListener("click", function(event) {
     event.preventDefault();
     let menu = document.querySelector(`#${menu_id}`);
     menu.classList.toggle(toggle_class);
-  })
+  });
 }
 
-// Toggle Listenerを追加してClickをListenする。
+// クリックをリッスンするトグルリスナーを追加する
 document.addEventListener("turbo:load", function() {
-  addToggleListener("hamburger", "navbar-menu",    "collapse");
-  addToggleListener("account",    "dropdown-menu", "active");
+  addToggleListener("hamburger", "navbar-menu",   "collapse");
+  addToggleListener("account",   "dropdown-menu", "active");
 });
