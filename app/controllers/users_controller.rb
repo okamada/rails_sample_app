87d2ae3a1cd1c69358ea45bca764adec219f9746
Hisashi Kamada <kamada.1994@gmail.com>
2023-11-19 13:17:36 +0000

    add access-control to user edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  before_action :logged_in_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
@@ -37,6 +39,14 @@ class UsersController < ApplicationController
   end
 
   private
+    # ログイン済ユーザーか？
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url, status: :see_other
+      end
+    end
+
     def user_params
       params
         .require(:user)
